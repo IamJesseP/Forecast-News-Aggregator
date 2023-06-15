071e429820127f32d2a4bcb761195c977621c92f
Jesse <jesseorhs@gmail.com>
2023-06-14 19:21:13 -0700

    feat: basic weather page strucutre

---
+++ frontend/src/WeatherPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import WeatherHero from './WeatherHero'
+import Forecast from './Forecast'
+
+export default function WeatherPage() {
+  return (
+    <div>
+      <WeatherHero/>
+      <Forecast />
+    </div>
+  )
+}
