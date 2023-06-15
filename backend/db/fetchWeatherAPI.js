9e3eb31cc288ac7dc141f08b3e33aaf241d50b4f
Jesse <jesseorhs@gmail.com>
2023-06-15 11:36:43 -0700

    feat: add geocoder
    This commit adds the node-geocoder package which utilizes the
    openstreetmap api to get geolocation data

--- backend/db/fetchWeatherAPI.js
+++ backend/db/fetchWeatherAPI.js
@@ -1,1 +1,17 @@
-let nodeGeocoder = require();
+const nodeGeocoder = require('node-geocoder');
+const options = {
+  provider: 'openstreetmap'
+};
+const geoCoder = nodeGeocoder(options);
+
+async function getGeocode(location) {
+  try {
+    const res = await geoCoder.geocode(location);
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+getGeocode('San Diego, CA');
