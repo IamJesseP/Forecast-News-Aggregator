aba62913438aaf8e03ec7b93b9452c3a56b90d43
Jesse <jesseorhs@gmail.com>
2023-06-15 12:18:40 -0700

    feat: fetch api with geocoder
    This commit adds a fetch to the openmeteo API using the longitude and
    latitude provided from the geocoder package.
    
    backend

--- backend/db/fetchWeatherAPI.js
+++ backend/db/fetchWeatherAPI.js
@@ -1,17 +1,35 @@
+// geocode package
 const nodeGeocoder = require('node-geocoder');
 const options = {
   provider: 'openstreetmap'
 };
 const geoCoder = nodeGeocoder(options);
 
+//axios
+const axios = require('axios');
+
 async function getGeocode(location) {
   try {
     const res = await geoCoder.geocode(location);
-    console.log(res);
+    return { latitude: res[1].latitude, longitude: res[1].longitude };
   } catch (err) {
     console.log(err);
     throw err;
   }
 }
 
-getGeocode('San Diego, CA');
+async function fetchWeatherAPI(location) {
+  try {
+    const geocode = await getGeocode(location);
+    const response = await axios.get(
+      `https://api.open-meteo.com/v1/forecast?latitude=${geocode.latitude}&longitude=${geocode.longitude}&hourly=temperature_2m,is_day&models=best_match&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FLos_Angeles`
+    );
+    const data = response.data;
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+fetchWeatherAPI('San Diego, CA');
