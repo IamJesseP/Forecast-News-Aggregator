7b43a21a5b72551fad89afc874bdac11e5f51db5
Jesse <jesseorhs@gmail.com>
2023-06-14 20:53:07 -0700

    chore: adjust files

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,23 @@
 // Express
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 // firebase
 const app = express();
 
-
 app.use(express.json());
 
 // Routers
-const taskRouter = require("./routes/taskRoutes");
+const taskRouter = require('./routes/taskRoutes');
 
 // Endpoints
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
-  });
+  res.send('Hello, world!');
+});
 
 // Start the server
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server is running`);
 });
-
