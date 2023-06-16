f1f72ac3ccd7a629c657b9eab4fcc3c241a2b22a
Jesse <jesseorhs@gmail.com>
2023-06-16 15:49:33 -0700

    feat: add temperature with icon to hero
    This commit pulls the temperature array from the weather object and sets
    the temperature to the herocard based off the current users time.
    
    frontend

--- backend/API/fetchWeatherAPI.js
+++ backend/API/fetchWeatherAPI.js
@@ -6,7 +6,7 @@ async function fetchWeatherAPI(location) {
   try {
     const geocode = await getGeocode(location);
     const response = await axios.get(
-      `https://api.open-meteo.com/v1/forecast?latitude=${geocode.latitude}&longitude=${geocode.longitude}&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto`
+      `https://api.open-meteo.com/v1/forecast?latitude=${geocode.latitude}&longitude=${geocode.longitude}&hourly=temperature_2m,rain,snowfall,cloudcover_high&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&forecast_days=1&timezone=auto`
     );
     const data = response.data;
     return data;
--- frontend/src/WeatherHero.js
+++ frontend/src/WeatherHero.js
@@ -3,8 +3,12 @@ import React from 'react';
 import { Container } from '@mui/material';
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import WbTwilightIcon from '@mui/icons-material/WbTwilight';
+import sunny from './assets/sunny.png';
+import cloudy from './assets/cloudy.png';
+import rainy from './assets/rainy.png';
 
 export default function WeatherHero({ weatherData, city }) {
+  // Sunrise and Sunset
   let sunriseArray = weatherData?.weatherData?.daily?.sunrise;
   let sunsetArray = weatherData?.weatherData?.daily?.sunset;
   let sunriseTime = new Date(sunriseArray[0]).toLocaleTimeString('en-US', {
@@ -17,17 +21,40 @@ export default function WeatherHero({ weatherData, city }) {
     minute: 'numeric',
     hour12: true
   });
+
+  // Max / low temps
+  let maxTemp = weatherData?.weatherData?.daily?.temperature_2m_max[0];
+  let lowTemp = weatherData?.weatherData?.daily?.temperature_2m_min[0];
+
+  // Curre
+  const currentHour = new Date().getHours();
+  const currentTemp = weatherData?.weatherData?.hourly?.temperature_2m[currentHour];
+  console.log(currentTemp);
   return (
     <div className="hero">
       <div className="hero-card">
-        <h1>{city}</h1>
-        <div className="hero-sun-time">
-          <h2>
-            <WbSunnyIcon /> {sunriseTime}
-          </h2>
-          <h2>
-            <WbTwilightIcon /> {sunsetTime}
-          </h2>
+        <div className="hero-container-1">
+          <h1>{city}</h1>
+          <div className="hero-sun-time">
+            <h2>
+              <WbSunnyIcon /> {sunriseTime}
+            </h2>
+            <h2>
+              <WbTwilightIcon /> {sunsetTime}
+            </h2>
+          </div>
+          <div className="hero-low-high">
+            <h2>{`L ${lowTemp}°`}</h2>
+            <h2>{`H ${maxTemp}°`}</h2>
+          </div>
+        </div>
+        <div className="hero-container-2">
+          <div>
+            <img src={sunny}></img>
+            {/* <img src={rainy}></img>
+            <img src={cloudy}></img> */}
+            <h1>{currentTemp}</h1>
+          </div>
         </div>
       </div>
     </div>
