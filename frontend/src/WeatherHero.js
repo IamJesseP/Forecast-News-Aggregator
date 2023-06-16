fd320ca171852e7e6f268dc8091c97caafd82baa
Jesse <jesseorhs@gmail.com>
2023-06-16 16:55:01 -0700

    feat: dynamically render hero card
    This commit adds the logic to render the weather dynamically based off
    specific values received from the weather object. An image and weather
    status are conditionally rendered on the right side.
    
    frontend

--- frontend/src/WeatherHero.js
+++ frontend/src/WeatherHero.js
@@ -6,6 +6,9 @@ import WbTwilightIcon from '@mui/icons-material/WbTwilight';
 import sunny from './assets/sunny.png';
 import cloudy from './assets/cloudy.png';
 import rainy from './assets/rainy.png';
+import snowy from './assets/snowy.png';
+import { motion } from 'framer-motion';
+import { fadeIn } from './variants';
 
 export default function WeatherHero({ weatherData, city }) {
   // Sunrise and Sunset
@@ -26,14 +29,59 @@ export default function WeatherHero({ weatherData, city }) {
   let maxTemp = weatherData?.weatherData?.daily?.temperature_2m_max[0];
   let lowTemp = weatherData?.weatherData?.daily?.temperature_2m_min[0];
 
-  // Curre
+  // Current temp
   const currentHour = new Date().getHours();
   const currentTemp = weatherData?.weatherData?.hourly?.temperature_2m[currentHour];
-  console.log(currentTemp);
+
+  // Cloudy Checker
+  let cloudWeather;
+  let cloudCoverArray = weatherData?.weatherData?.hourly?.cloudcover_high;
+  let cloudSum = cloudCoverArray.reduce((a, b) => a + b, 0);
+  let cloudAvg = cloudSum / cloudCoverArray.length;
+  // not an actual metric right here:
+  if (cloudAvg <= 15) {
+    cloudWeather = false;
+  } else {
+    cloudWeather = true;
+  }
+
+  // Rainy Checker
+  let rainWeather;
+  let rainCoverArray = weatherData?.weatherData?.hourly?.rain;
+  let rainSum = rainCoverArray.reduce((a, b) => a + b, 0);
+  let rainAvg = rainSum / rainCoverArray.length;
+  // not an actual metric right here:
+  if (rainAvg <= 0.004) {
+    rainWeather = false;
+  } else {
+    rainWeather = true;
+  }
+
+  // Snowy Checker
+  let snowWeather;
+  let snowCoverArray = weatherData?.weatherData?.hourly?.snowfall;
+  let snowSum = snowCoverArray.reduce((a, b) => a + b, 0);
+  let snowAvg = snowSum / snowCoverArray.length;
+  // not an actual metric right here:
+  if (snowAvg <= 15) {
+    snowWeather = false;
+  } else {
+    snowWeather = true;
+  }
   return (
-    <div className="hero">
+    <motion.div
+      className="hero"
+      variants={fadeIn('right', 0.3)}
+      initial="hidden"
+      whileInView={'show'}
+      viewport={{ once: false, amount: 0.5 }}>
       <div className="hero-card">
-        <div className="hero-container-1">
+        <motion.div
+          className="hero-container-1"
+          variants={fadeIn('right', 0.3)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.5 }}>
           <h1>{city}</h1>
           <div className="hero-sun-time">
             <h2>
@@ -47,16 +95,29 @@ export default function WeatherHero({ weatherData, city }) {
             <h2>{`L ${lowTemp}°`}</h2>
             <h2>{`H ${maxTemp}°`}</h2>
           </div>
-        </div>
+        </motion.div>
         <div className="hero-container-2">
-          <div>
-            <img src={sunny}></img>
-            {/* <img src={rainy}></img>
-            <img src={cloudy}></img> */}
-            <h1>{currentTemp}</h1>
-          </div>
+          <motion.div
+            variants={fadeIn('right', 0.3)}
+            initial="hidden"
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.5 }}>
+            {!cloudWeather && !rainWeather && !snowWeather && <img src={sunny}></img>}
+            {cloudWeather && !rainWeather && !snowWeather && <img src={cloudy}></img>}
+            {rainWeather && !snowWeather && <img src={rainy}></img>}
+            {snowWeather && <img src={snowy}></img>}
+            <div>
+              <h1>{`${currentTemp}°`}</h1>
+              <h1 className="hero-current-weather">
+                {!cloudWeather && !rainWeather && !snowWeather && 'Sunny'}
+                {cloudWeather && !rainWeather && !snowWeather && 'Cloudy'}
+                {rainWeather && !snowWeather && 'Rainy'}
+                {snowWeather && 'Snowy'}
+              </h1>
+            </div>
+          </motion.div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
