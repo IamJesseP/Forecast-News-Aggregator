e89e5a650f5c0b2a6a22e0f99fcee1e2546a971c
Jesse <jesseorhs@gmail.com>
2023-06-16 21:43:59 -0700

    feat: finish second card in forecast

--- frontend/src/Forecast.js
+++ frontend/src/Forecast.js
@@ -5,6 +5,7 @@ import WbTwilightIcon from '@mui/icons-material/WbTwilight';
 import forecastSun from './assets/forecast-sun.png';
 import forecastSunrise from './assets/forecast-sunrise.png';
 import forecastSunset from './assets/forecast-sunset.png';
+import dailyRain from './assets/daily-rain.png';
 import React from 'react';
 
 export default function Forecast({ weatherData }) {
@@ -22,7 +23,8 @@ export default function Forecast({ weatherData }) {
     hour12: true
   });
 
-  let indexArray = weatherData?.weatherData?.daily?.uv_index_max[0];
+  let uvIndexArray = weatherData?.weatherData?.daily?.uv_index_max[0];
+  let dailyRainfall = weatherData?.weatherData?.daily?.rain_sum[0];
   return (
     <div className="forecast">
       <div className="forecast-left">
@@ -31,7 +33,7 @@ export default function Forecast({ weatherData }) {
             <img src={forecastSun} width="36px"></img>
             <div>
               <h2>UV Index</h2>
-              <h3>{indexArray} of 10</h3>
+              <h3>{uvIndexArray} of 10</h3>
             </div>
           </div>
           <div className="forecast-divider"></div>
@@ -44,10 +46,18 @@ export default function Forecast({ weatherData }) {
             </h2>
           </div>
         </div>
-        <div className="card-left-2">Forecast 2</div>
+        <div className="card-left-2">
+          <div className="daily-rain-left">
+            <h2>Daily rainfall</h2>
+            <h3>{dailyRainfall} inches</h3>
+          </div>
+          <div className="daily-rain-right">
+            <img src={dailyRain} width="36px"></img>
+          </div>
+        </div>
       </div>
       <div className="forecast-right">
-        <div className="card-right"> Air Quality</div>
+        <div className="card-right">Air Quality</div>
       </div>
     </div>
   );
