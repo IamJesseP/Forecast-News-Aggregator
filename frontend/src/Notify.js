cdfe6feabd738e3ae464cfdfd3e9e76332c2b38a
Jesse <jesseorhs@gmail.com>
2023-06-17 18:03:27 -0700

    feat: implement notify form styling and input

--- frontend/src/Notify.js
+++ frontend/src/Notify.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 export default function Notify() {
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // handle the submit logic
+  };
+
   return (
     <div className="notify">
-      <div className="hero-card">
-        <h1>Get notified</h1>
+      <div className="notify-title">
+        <h1></h1>
+
+        <h1></h1>
+      </div>
+      <div className="notify-card">
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            maxWidth: 300,
+            m: 'auto',
+            gap: 2
+          }}>
+          <h1 className="notify-title">Daily Weather Notifications</h1>
+          <TextField
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            label="City"
+            className="notify-form"
+          />
+          <TextField
+            value={state}
+            onChange={(e) => setState(e.target.value)}
+            label="State"
+            className="notify-form"
+          />
+          <TextField
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            label="Phone Number"
+            className="notify-form"
+          />
+          <Button variant="contained" type="submit">
+            Submit
+          </Button>
+        </Box>
       </div>
     </div>
   );
