25bd4fdc5db6607163d6c0e4733e8383a13f67f4
Jesse <jesseorhs@gmail.com>
2023-06-19 11:28:18 -0700

    feat:implement unsubscribe to notify.js

--- frontend/src/Notify.js
+++ frontend/src/Notify.js
@@ -11,20 +11,29 @@ export default function Notify() {
   const [state, setState] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [phoneResponse, setPhoneResponse] = useState('');
+  const [phoneForm, setPhoneForm] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const url = 'https://the-weather-news-aggr-b8c24050aa8e.herokuapp.com/phone/savephone'; // Replace with your API url
-
+    const saveUrl = 'https://the-weather-news-aggr-b8c24050aa8e.herokuapp.com/phone/savephone';
+    const deleteUrl = 'https://the-weather-news-aggr-b8c24050aa8e.herokuapp.com/phone/deletephone';
     try {
-      const response = await axios.post(url, {
-        city,
-        state,
-        phoneNumber
-      });
-      const responseData = response.data.msg;
-      setPhoneResponse(responseData);
+      if (!phoneForm) {
+        const response = await axios.post(saveUrl, {
+          city,
+          state,
+          phoneNumber
+        });
+        const responseData = response.data.msg;
+        setPhoneResponse(responseData);
+      } else {
+        const response = await axios.delete(deleteUrl, {
+          phoneNumber
+        });
+        const responseData = response.data.msg;
+        setPhoneResponse(responseData);
+      }
     } catch (error) {
       console.log('An error occurred while saving the data:', error);
     }
@@ -50,28 +59,45 @@ export default function Notify() {
             gap: 2
           }}>
           <h1 className="notify-title">Daily Weather Notifications</h1>
-          <TextField
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            label="City"
-            className="notify-form"
-          />
-          <TextField
-            value={state}
-            onChange={(e) => setState(e.target.value)}
-            label="State"
-            className="notify-form"
-          />
+          {!phoneForm && (
+            <>
+              <TextField
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                label="City"
+                className="notify-form"
+                required
+              />
+              <TextField
+                value={state}
+                onChange={(e) => setState(e.target.value)}
+                label="State"
+                className="notify-form"
+                required
+              />
+            </>
+          )}
           <TextField
             value={phoneNumber}
             onChange={(e) => setPhoneNumber(e.target.value)}
             label="Phone Number"
             className="notify-form"
+            required
           />
           <Button variant="contained" type="submit" onClick={handleSubmit}>
-            Submit
+            {!phoneForm && 'Subscribe'}
+            {phoneForm && 'Unsubscribe'}
           </Button>
-          {phoneResponse && <h3>{phoneResponse}</h3>}
+          <h3 style={{ color: 'black', maxWidth: '150px' }}>
+            <a
+              onClick={() => {
+                setPhoneForm((prevState) => !prevState);
+              }}>
+              {!phoneForm && 'Unsubscribe here'}
+              {phoneForm && 'Sign up here'}
+            </a>
+          </h3>
+          {phoneResponse && <h3 style={{ color: 'black', maxWidth: '150px' }}>{phoneResponse}</h3>}
         </Box>
       </div>
     </div>
