9a4d5df10133e5b3fa93a36db76da3ec35694e94
Jesse <jesseorhs@gmail.com>
2023-06-19 08:39:03 -0700

    build: adjust to connect to production backend

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const AWS = require('./db/config');
 //Security
 const cors = require('cors');
-
 app.use(cors());
 app.use(express.json());
 
